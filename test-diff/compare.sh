fcf77141c2836322509081d0d8a6c3347318f8e2
Martin Chuka Chinaka <martinchukachinaka@gmail.com>
2022-08-12 05:17:05 +0200

    apply method change to script

--- test-diff/compare.sh
+++ test-diff/compare.sh
@@ -3,8 +3,7 @@ for file in input-files/heavy-table.json
 do 
   echo Generating XLSX...\\n
   EXEC_TIME_XLSX_START=`date +%s%N | cut -b1-13`
-  curl -s POST --data @"$file" 'http://localhost:3008/json2xlsx' \
-   --header 'Content-Type:application/json' \
+  curl -s GET 'http://localhost:3008/json2xlsx' \
    --verbose > output-files/result.xlsx
   EXEC_TIME_XLSX_END=`date +%s%N | cut -b1-13`
 
@@ -13,8 +12,7 @@ do
 
   echo Generating PDF...\\n
   EXEC_TIME_PDF_START=`date +%s%N | cut -b1-13`
-  curl -s POST --data @"$file" 'http://localhost:3008/json2pdf' \
-   --header 'Content-Type:application/json' \
+  curl -s GET 'http://localhost:3008/json2pdf' \
    --verbose > output-files/result.pdf
   EXEC_TIME_PDF_END=`date +%s%N | cut -b1-13`
 
